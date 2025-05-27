b376a370041c5c9824a9ac932ba774b12b7ceab9
itsmandrew <theachang1688@gmail.com>
2025-05-26 18:01:17 -0700

    Added help function

--- commands/commands.go
+++ commands/commands.go
@@ -17,11 +17,36 @@ func init() {
 			Description: "Exits the CLI",
 			Callback:    func(args []string) error { return CommandExit(args) },
 		},
+		"help": {
+			Name:        "help",
+			Description: "Displays a help message",
+			Callback:    func(args []string) error { return CommandHelp(args) },
+		},
 	}
 
 }
 
 func CommandExit(args []string) error {
+	if len(args) > 0 {
+		return e.ErrTooManyArgs
+	}
 	fmt.Println("Closing the CEFI CLI... Goodbye!")
 	return e.ErrExit
+}
+
+func CommandHelp(args []string) error {
+	if len(args) > 0 {
+		return e.ErrTooManyArgs
+	}
+
+	fmt.Println()
+	fmt.Println("Usage:")
+
+	for cmd, val := range CommandTable {
+		fmt.Printf("  %s: %v\n", cmd, val.Description)
+	}
+
+	fmt.Println()
+
+	return nil
 }
--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,5 +3,6 @@ package errors
 import "errors"
 
 var (
-	ErrExit = errors.New("exit requested")
+	ErrExit        = errors.New("exit requested")
+	ErrTooManyArgs = errors.New("too many arguments for given command")
 )
